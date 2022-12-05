28643cd02b46d865eaf9f99f0b2ebec47b0c5932
dcartist <pbannerman13@gmail.com>
2022-12-05 18:05:19 -0500

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    // serverURL: 'http://localhost:8080/api'
     serverURL: 'https://still-woodland-20464.herokuapp.com/api/old'
+    // serverURL: 'http://localhost:8080/api'
     // serverURL: 'https://flickpick-api.herokuapp.com/api'
 }
